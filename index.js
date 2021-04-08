5fc5959d8352e182ae29be3139708cdb3cc8fa15
gerpan <nhaths4701@gmail.com>
2021-04-08 18:19:47 +0700

    fix error

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const app = require('./src/server');
 const config = require('./src/config');
 
 const PORT = config.serverSettings.port || 8888;
-app.listen(process.env.PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
