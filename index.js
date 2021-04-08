708a37478bf9e427f632bd2fda4c5041aff26cdd
gerpan <nhaths4701@gmail.com>
2021-04-08 17:54:41 +0700

    update

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const app = require('./src/server');
 const config = require('./src/config');
 
 const PORT = config.serverSettings.port || 8888;
-console.log(process.env)
 app.listen(process.env.PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
